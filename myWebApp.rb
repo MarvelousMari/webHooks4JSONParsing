58d1873750b94d3840abde18c5376670093d01ff
MarvelousMari <mariella@mariellapage.com>
2020-07-25 17:01:54 -0500

    Test push

--- myWebApp.rb
+++ myWebApp.rb
@@ -1,13 +1,20 @@
 require 'sinatra'
 require 'json'
 
+#set :port, 443
+#set :port, 3030
+#set :port, 8080
+#set :port, 4040
+#default is 4567
+set :bind, 'webHooks4JSONParsing.expose'
 
+print "running Ruby Script\n"
 # catches payload push and gets the JSON
 post '/payload' do
+#post '/' do
+  print "in payload\n"
   push = JSON.parse(request.body.read)
-  puts "I got some JSON: #{push.inspect}"
+  puts "I got some JSON! #{push.inspect}"
 end
-
-# push test 4
 
 # TODO: need to add SECRET to secure webhook
