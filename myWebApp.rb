53c986cd1f5cb41623b602b389802960c372b6e6
MarvelousMari <mariella@mariellapage.com>
2020-07-25 17:22:24 -0500

    added secret token

--- myWebApp.rb
+++ myWebApp.rb
@@ -8,13 +8,17 @@ require 'json'
 #default is 4567
 set :bind, 'webHooks4JSONParsing.expose'
 
-print "running Ruby Script\n"
-# catches payload push and gets the JSON
 post '/payload' do
-#post '/' do
-  print "in payload\n"
-  push = JSON.parse(request.body.read)
-  puts "I got some JSON! #{push.inspect}"
+  request.body.rewind
+  payload_body = request.body.read
+  verify_signature(payload_body)
+  push = JSON.parse(params[:payload])
+  "I got some JSON: #{push.inspect}"
+end
+
+def verify_signature(payload_body)
+  signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
+  return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
 end
 
 # TODO: need to add SECRET to secure webhook
