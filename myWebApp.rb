11271d557b8d26a84977c7712b0a8b5b9df7c645
MarvelousMari <mariella@mariellapage.com>
2020-07-27 15:51:00 -0500

    Functioning HTML generation may need to test further

--- myWebApp.rb
+++ myWebApp.rb
@@ -6,9 +6,7 @@ require 'pp'
 #set :port, 8080
 #default is 4567
 set :bind, 'webHooks4JSONParsing.expose'
-
-# (0) sets the default value for key's with no value
-$commitInfo = Hash.new(0)
+$commitInfo = Hash.new()
 # looks like
 # {
 #   repository 'name' => {
@@ -42,17 +40,28 @@ get '/commitHTML' do
 end
 
 def getHTML()
-
-  return halt 200, "got the GET"
+  html2Return = "<ul>\n\t"
+  # get the repo name create a link with the message for text and connect it to repository link
+  # and set that to the first list line
+  $commitInfo.each{|repository, repoVal| html2Return += "<li><a href=\"" + repoVal['repoURL'] + "\">" + repository + "</a><ul>\n"
+    # get each commit first line message and set it as a link to the commit page
+    # then added it as a sub bullet to the repoName list
+    repoVal['commits'].each{|message, commitURL| html2Return += "\t\t" + "<li><a href=\"" + commitURL + "\">" + message + "</a></li>\n"}
+    # end the list
+    html2Return += "\t</ul></li>\n"}
+  # end the bloc
+  html2Return += "</ul>"
+  puts html2Return
+  return halt 200, html2Return
 end
 
 def getPushInfo(push_hash)
   # check if there is a repo with this name in the hash already
-  if $commitInfo[push_hash['repository']['name']] = 0
+  if $commitInfo[push_hash['repository']['name']].nil?
     # add new repo info
-    $commitInfo[push_hash['repository']['name']] = Hash.new(0)
+    $commitInfo[push_hash['repository']['name']] = Hash.new()
     $commitInfo[push_hash['repository']['name']]['repoURL'] = push_hash['repository']['owner']['url']
-    $commitInfo[push_hash['repository']['name']]['commits'] = Hash.new(0)
+    $commitInfo[push_hash['repository']['name']]['commits'] = Hash.new()
   end
 
   # get each commit's url and message
@@ -62,10 +71,11 @@ def getPushInfo(push_hash)
     $commitInfo[push_hash['repository']['name']]['commits'][messageString] = commit['url']
   end
   pp $commitInfo
-  return halt 200, "worked"
+  return halt 200, "got push info"
 
 end
 
+# check SECRET_TOKEN Enviorment/server variable
 def verify_signature(payload_body)
   signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
   return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
