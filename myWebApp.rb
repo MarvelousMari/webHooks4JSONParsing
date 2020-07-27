b6d10d69b735fc471e333abb6db191261a939370
MarvelousMari <mariella@mariellapage.com>
2020-07-27 09:48:01 -0500

    Pretty Formated JSON

--- myWebApp.rb
+++ myWebApp.rb
@@ -2,22 +2,25 @@ require 'sinatra'
 require 'json'
 
 #set :port, 443
-#set :port, 3030
 #set :port, 8080
-#set :port, 4040
 #default is 4567
 set :bind, 'webHooks4JSONParsing.expose'
 
 post '/payload' do
   request.body.rewind
+  print "\n ONE \n"
   payload_body = request.body.read
+  print "\n TWO \n"
   verify_signature(payload_body)
-  push = JSON.parse(params[:payload])
-  "I got some JSON: #{push.inspect}"
+  print "\n FOUR \n"
+  push = JSON.parse(payload_body)
+  print "\n FIVE \n"
+  puts JSON.pretty_generate(push)
 end
 
 def verify_signature(payload_body)
   signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
+  print "\n THREE \n"
   return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
 end
 
