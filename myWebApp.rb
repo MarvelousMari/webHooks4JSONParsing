cc09d82ad97c3ee96b653e04627e01a85a5d46f7
MarvelousMari <mariella@mariellapage.com>
2020-07-27 13:43:19 -0500

    Fixed Multiline Commit Message, Framed GET Response also added a visual demo of the format of the commitInfo

--- myWebApp.rb
+++ myWebApp.rb
@@ -1,10 +1,31 @@
 require 'sinatra'
 require 'json'
+require 'pp'
 
 #set :port, 443
 #set :port, 8080
 #default is 4567
 set :bind, 'webHooks4JSONParsing.expose'
+
+# (0) sets the default value for key's with no value
+$commitInfo = Hash.new(0)
+# looks like
+# {
+#   repository 'name' => {
+#     repoURL
+#     commits{
+#       {'message'(first line) => 'commitURL'}
+#       {'secondCommitMessage' => www.githubYADAYADA}
+#     }
+#   }
+#   repository2 'name' => {
+#     repoURL
+#     commits{
+#       {'message'(first line) => 'commitURL'}
+#       {'secondCommitMessage' => www.githubYADAYADA}
+#     }
+#   }
+# }
 
 post '/payload' do
   request.body.rewind
@@ -13,25 +34,39 @@ post '/payload' do
   verify_signature(payload_body)
   # convert JSON to Ruby hash
   push = JSON.parse(payload_body)
-  getCommitInfo(push)
+  getPushInfo(push)
 end
 
-def getCommitInfo(push_hash)
-  # get Repository name
-  print "\n" + push_hash['repository']['name'] + "\n"
-  # get the url for the repository
-  print push_hash['repository']['owner']['url'] + "\n"
+get '/commitHTML' do
+  getHTML()
+end
+
+def getHTML()
+
+  return halt 200, "got the GET"
+end
+
+def getPushInfo(push_hash)
+  # check if there is a repo with this name in the hash already
+  if $commitInfo[push_hash['repository']['name']] = 0
+    # add new repo info
+    $commitInfo[push_hash['repository']['name']] = Hash.new(0)
+    $commitInfo[push_hash['repository']['name']]['repoURL'] = push_hash['repository']['owner']['url']
+    $commitInfo[push_hash['repository']['name']]['commits'] = Hash.new(0)
+  end
+
   # get each commit's url and message
   for commit in push_hash['commits'] do
-    print "\n" + commit['url'] + "\n"
-    print commit['message']
-    print "\n" + "\n"
+    # get only the first line of the commit message
+    messageString = commit['message'].split("\n")[0]
+    $commitInfo[push_hash['repository']['name']]['commits'][messageString] = commit['url']
   end
+  pp $commitInfo
   return halt 200, "worked"
+
 end
 
 def verify_signature(payload_body)
   signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
-  print "\n THREE \n"
   return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
 end
