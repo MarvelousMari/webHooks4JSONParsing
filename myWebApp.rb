1fde8567489abe8a9659cf752dd505c873e6ac1f
MarvelousMari <mariella@mariellapage.com>
2020-07-27 17:11:04 -0500

    Fixed Url Issue

--- myWebApp.rb
+++ myWebApp.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'json'
+# pretty print
 require 'pp'
 
 #set :port, 443
@@ -8,22 +9,23 @@ require 'pp'
 set :bind, 'webHooks4JSONParsing.expose'
 $commitInfo = Hash.new()
 # looks like
-# {
-#   repository 'name' => {
-#     repoURL
-#     commits{
-#       {'message'(first line) => 'commitURL'}
-#       {'secondCommitMessage' => www.githubYADAYADA}
-#     }
-#   }
-#   repository2 'name' => {
-#     repoURL
-#     commits{
-#       {'message'(first line) => 'commitURL'}
-#       {'secondCommitMessage' => www.githubYADAYADA}
-#     }
-#   }
-# }
+# {"appWindowSetUp"=>
+#   {"repoURL"=>"https://api.github.com/users/MarvelousMari",
+#    "commits"=>
+#     {"Updated Bash Launch Github URL"=>
+#       "https://github.com/MarvelousMari/appWindowSetUp/commit/de4b18f94a2c3488055d839264242098601fb2d1",
+#      "Updated Android Launch Github URL"=>
+#       "https://github.com/MarvelousMari/appWindowSetUp/commit/368b6dda44b90617edbb530a2d127f1687c16dd5",
+#      "More Recent Generated Script"=>
+#       "https://github.com/MarvelousMari/appWindowSetUp/commit/1dbb6e95d3a2e5c3fddaf377c37699b4bb785aac"}},
+#  "webHooks4JSONParsing"=>
+#   {"repoURL"=>"https://api.github.com/users/MarvelousMari",
+#    "commits"=>
+#     {"Improved formating of Demo Block HTML"=>
+#       "https://github.com/MarvelousMari/webHooks4JSONParsing/commit/1f8a396142e193c7093bb765718ea1ef94da0e76",
+#      "Functioning HTML generation"=>
+#       "https://github.com/MarvelousMari/webHooks4JSONParsing/commit/11271d557b8d26a84977c7712b0a8b5b9df7c645"}}}
+
 
 post '/payload' do
   request.body.rewind
@@ -52,6 +54,7 @@ def getHTML()
   # end the bloc
   html2Return += "</ul>"
   puts html2Return
+  $commitInfo.clear
   return halt 200, html2Return
 end
 
@@ -60,7 +63,7 @@ def getPushInfo(push_hash)
   if $commitInfo[push_hash['repository']['name']].nil?
     # add new repo info
     $commitInfo[push_hash['repository']['name']] = Hash.new()
-    $commitInfo[push_hash['repository']['name']]['repoURL'] = push_hash['repository']['owner']['url']
+    $commitInfo[push_hash['repository']['name']]['repoURL'] = push_hash['html_url']
     $commitInfo[push_hash['repository']['name']]['commits'] = Hash.new()
   end
 
@@ -72,7 +75,6 @@ def getPushInfo(push_hash)
   end
   pp $commitInfo
   return halt 200, "got push info"
-
 end
 
 # check SECRET_TOKEN Enviorment/server variable
